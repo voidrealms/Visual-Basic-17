6a8432bacb9bdd42c46e8beb312765d060df90bc
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:40:09 -0400

    First commit

---
+++ VB 17/ConsApp stack 17/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'stack list in is first out like plates
+
+        Dim papers As Stack = New Stack
+
+        papers.Push("resume")
+        papers.Push("TPS report")
+        papers.Push("to do list")
+
+        Console.WriteLine(papers.Pop)
+        Console.WriteLine(papers.Pop)
+        Console.WriteLine(papers.Pop)
+        Console.WriteLine(papers.Count)
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual Basic 17 - Stack
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=SP1Hl98wfpM
+
+Description: learn about the FILO (First in Last out) Stack class
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
